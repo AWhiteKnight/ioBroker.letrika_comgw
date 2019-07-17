63fac06016087525d7d868ea2c9a3c39922e835c
AWhiteKnight <awhiteknight@unity-mail.de>
2019-07-17 14:36:50 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'letrika_comgw adapter settings': {
+		'en': 'Adapter settings for letrika_comgw',
+		'de': 'Adaptereinstellungen für letrika_comgw',
+		'ru': 'Настройки адаптера для letrika_comgw',
+		'pt': 'Configurações do adaptador para letrika_comgw',
+		'nl': 'Adapterinstellingen voor letrika_comgw',
+		'fr': "Paramètres d'adaptateur pour letrika_comgw",
+		'it': "Impostazioni dell'adattatore per letrika_comgw",
+		'es': 'Ajustes del adaptador para letrika_comgw',
+		'pl': 'Ustawienia adaptera dla letrika_comgw',
+		'zh-cn': 'letrika_comgw的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
