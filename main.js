579a1ddfd4b706930a9d10e7e99d6bcffc0b9f86
AWhiteKnight <awhiteknight@unity-mail.de>
2019-07-22 20:43:53 +0200

    first values

--- main.js
+++ main.js
@@ -26,6 +26,24 @@ const http = require('http');
 const https = require('https');
 let interval;
 
+let adapter;
+
+async function intervalHandler() {
+	// read inverter data
+	adapter.log.debug('reading inverter');
+	const reqPanelData = {
+		host: adapter.config.comgwIp,
+		port: adapter.config.comgwPort,
+		path: '/get_inverter_info',
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
+	adapter.getJSON(reqPanelData, (result) => {
+		adapter.handleInverterInfo(JSON.parse(result), false);
+	});
+}
 
 class LetrikaComgw extends utils.Adapter {
 
@@ -49,6 +67,7 @@ class LetrikaComgw extends utils.Adapter {
 	 */
 	async onReady() {
 		this.setStateChanged('info.connection', false, true);
+		adapter = this;
 		// Initialize your adapter here
 		try {
 			this.log.debug('starting letrika_comgw');
@@ -62,27 +81,21 @@ class LetrikaComgw extends utils.Adapter {
 				}
 			};
 			// read general system information
-			request.path = '/system_info';
+			request.path = '/get_initial';
 			this.getJSON(request, (result) => {
-				this.log.debug(result);
-				// read server settings
-				request.path ='/get_settings';
+				const obj1 = JSON.parse(result);
+				this.log.debug(JSON.stringify(obj1));
+				// volatile data
+				request.path ='/get_changes';
 				this.getJSON(request, (result) => {
-					this.log.debug(result);
+					const obj2 = JSON.parse(result);
+					// this.log.debug(JSON.stringify(obj2));
+					this.handleInverterInfo(obj2.inverter_info, true);
+					// this.log.debug(JSON.stringify(obj2.plant_info));
+					// this.log.debug(JSON.stringify(obj2.alarm_history));
 					// read available inverters
-					request.path ='/get_inverter_info';
-					this.getJSON(request, (result) => {
-						this.log.debug(result);
-						// read inverter measurement this needs to be done in a loop!
-						request.path = '/get_measurements';
-						const handler = this.getJSON(request, (result) => {
-							this.log.debug(result);
-						});
-						// @ts-ignore
-						interval = setInterval(handler,	this.config.comgwInterval * 60000);
-						this.setStateChanged('info.connection', true, true);
-
-					});
+					interval = setInterval(intervalHandler,	this.config.comgwInterval * 60000);
+					this.setStateChanged('info.connection', true, true);
 				});
 			});
 		} catch(err) {
@@ -152,20 +165,48 @@ class LetrikaComgw extends utils.Adapter {
 	// 	}
 	// }
 
+	handleInverterInfo(info, init) {
+		// this.log.debug(JSON.stringify(info));
+		info.forEach(element => {
+			if(init) {
+				const helper = require(__dirname + '/lib/helper.js');
+				helper.createInverterEntries(adapter, element);
+			}
+			// this.log.debug('updating ' + element.inverter);
+			this.setStateChanged(element.inverter + 'hw_version', {val: element.hw_version, ack: true});
+			this.setStateChanged(element.inverter + 'sw_version_pri', {val: element.sw_version_pri, ack: true});
+			this.setStateChanged(element.inverter + 'sw_version_sec', {val: element.sw_version_sec, ack: true});
+			this.setStateChanged(element.inverter + 'max_power', {val: element.max_power, ack: true});
+			this.setStateChanged(element.inverter + 'protocol_ver', {val: element.protocol_ver, ack: true});
+			this.setStateChanged(element.inverter + 'status', {val: element.status == 'working' ? true : false, ack: true});
+
+			this.setStateChanged(element.inverter + '.measurement.energy', {val: element.measurement.energy, ack: true});
+			this.setStateChanged(element.inverter + '.measurement.power', {val: element.measurement.power, ack: true});
+
+			this.setStateChanged(element.inverter + '.details.max_power_soft', {val: element.details.max_power_soft, ack: true});
+			this.setStateChanged(element.inverter + '.details.cos_phi', {val: element.details.cos_phi, ack: true});
+			this.setStateChanged(element.inverter + '.details.power_status', {val: element.details.power_status == 'on' ? true : false, ack: true});
+			this.setStateChanged(element.inverter + '.details.grid_freq', {val: element.details.grid_freq, ack: true});
+			this.setStateChanged(element.inverter + '.details.dcdc_temp', {val: element.details.dcdc_temp, ack: true});
+			this.setStateChanged(element.inverter + '.details.dcac_temp', {val: element.details.dcac_temp, ack: true});
+			this.setStateChanged(element.inverter + '.details.sec_dc_voltage', {val: element.details.sec_dc_voltage, ack: true});
+			this.setStateChanged(element.inverter + '.details.alarm', {val: element.details.alarm, ack: true});
+		});
+	}
+
 	getJSON(options, cbOnResult) {
 		const port = options.port == 443 ? https : http;
 
 		let output = '';
 
 		const req = port.request(options, (res) => {
-			res.setEncoding('utf8');
+			// res.setEncoding('utf8');
 
 			res.on('data', (chunk) => {
 				output += chunk;
 			});
 
 			res.on('end', () => {
-				this.log.debug(res.statusCode + ':' + output);
 				if(res.statusCode == 200) {
 					cbOnResult(output);
 				} else {
